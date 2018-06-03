15ef0e414210fbe427018c56a460d96138a99ee6
lapumbas <lapumbas@ya.ru>
2018-06-03 10:14:43 +0300

    minor updates

--- js/pictures.js
+++ js/pictures.js
@@ -1,11 +1,11 @@
 let photos = [];
 let photosAmount = 25;
 
-function randomInt(min, max) {
+const randomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-function randomComments(arr) {
+const randomComments = (arr) => {
   let temporaryArr = [...arr];
   let result = [];
   for (let i = 0; i < randomInt(1, 3); i++) {
@@ -31,32 +31,65 @@ for (let i = 0; i < photosAmount; i++) {
   photos[i].comments = randomComments(comments);
 }
 
+
 let template = document.querySelector(`#picture-template`).content;
 let pictures = document.querySelector(`.pictures`);
 let fragment = document.createDocumentFragment();
 let galleryOverlay = document.querySelector(`.gallery-overlay`);
 
-const showGalleryOverlay = (photo) => {
+const showGalleryOverlayHandler = (photo) => {
   galleryOverlay.querySelector(`.gallery-overlay-image`).src = photo.url;
   galleryOverlay.querySelector(`.likes-count`).innerText = String(photo.likes);
   galleryOverlay.querySelector(`.comments-count`).innerText = String(photo.comments.length);
   galleryOverlay.classList.remove(`hidden`);
 };
 
-
-let image = [];
+const closeGalleryOverlayHandler = (photo) => {
+  galleryOverlay.classList.add(`hidden`);
+};
 
 for (let i = 0; i < photos.length; i++) {
-  image[i] = template.cloneNode(true);
-  image[i].querySelector(`img`).src = photos[i].url;
-  image[i].querySelector(`.picture-likes`).innerText = String(photos[i].likes);
-  image[i].querySelector(`.picture-comments`).innerText = String(photos[i].comments.length);
-  fragment.appendChild(image[i]);
+  let image = template.cloneNode(true);
+  image.querySelector(`img`).src = photos[i].url;
+  image.querySelector(`.picture-likes`).innerText = String(photos[i].likes);
+  image.querySelector(`.picture-comments`).innerText = String(photos[i].comments.length);
+
+  fragment.appendChild(image);
 };
 pictures.appendChild(fragment);
 
-let rrrr = pictures.querySelectorAll('.picture');
-console.log(rrrr)
-rrrr.forEach(item=>{
-  item.addEventListener('')
-})
+let picture = pictures.querySelectorAll('.picture');
+
+console.log(picture);
+
+picture.forEach((item, index) => {
+  item.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    showGalleryOverlayHandler(photos[index]);
+  });
+});
+
+picture.forEach((item, index) => {
+  item.addEventListener('keydown', (evt) => {
+    if (evt.keyCode === 13) showGalleryOverlayHandler(photos[index]);
+  });
+});
+
+let galleryOverlayCloseButton = galleryOverlay.querySelector('.gallery-overlay-close');
+galleryOverlayCloseButton.tabIndex = '0';
+galleryOverlayCloseButton.addEventListener('click', evt => {
+  closeGalleryOverlayHandler();
+});
+
+if (!galleryOverlay.classList.contains('.hidden')) {
+  document.addEventListener('keydown', evt => {
+    if (evt.keyCode === 27) {
+      closeGalleryOverlayHandler();
+    }
+  });
+  galleryOverlayCloseButton.addEventListener('keydown', evt => {
+    if (evt.keyCode === 13) {
+      closeGalleryOverlayHandler();
+    }
+  })
+};
